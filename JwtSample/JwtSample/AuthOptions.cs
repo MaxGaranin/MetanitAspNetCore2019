8f2248749fc936210f42716141f718f2f8c079d3
GaraninMS <GaraninMS@samnipineft.ru>
2019-07-15 14:29:57 +0400

    Разбор примера WebAPI с JWT-токенами

---
+++ JwtSample/JwtSample/AuthOptions.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JwtSample
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "MyAuthServer";
+        public const string AUDIENCE = "https://localhost:44355/";
+        public const int LIFETIME = 1;
+
+        private const string KEY = "mysupersecret_secretkey!123";
+
+        public static SymmetricSecurityKey GetSymmetricSecurityKey()
+        {
+            return new SymmetricSecurityKey(Encoding.ASCII.GetBytes(KEY));
+        }
+    }
+}
---
+++ JwtSample/JwtSample/Controllers/AccountController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using JwtSample.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+
+namespace JwtSample.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly List<Person> _people = new List<Person>
+        {
+            new Person {Login = "admin@gmail.com", Password = "12345", Role = "admin"},
+            new Person {Login = "qwerty", Password = "55555", Role = "user"}
+        };
+
+        [HttpPost("/token")]
+        public async Task Token()
+        {
+            var userName = Request.Form["username"];
+            var password = Request.Form["password"];
+
+            var identity = GetIdentity(userName, password);
+            if (identity == null)
+            {
+                Response.StatusCode = 400;
+                await Response.WriteAsync("Invalid username or password.");
+                return;
+            }
+
+            var now = DateTime.UtcNow;
+
+            // создаем JWT-токен
+            var jwt = new JwtSecurityToken(
+                issuer: AuthOptions.ISSUER,
+                audience: AuthOptions.AUDIENCE,
+                notBefore: now,
+                claims: identity.Claims,
+                expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt,
+                username = identity.Name
+            };
+
+            Response.ContentType = "application/json";
+            await Response.WriteAsync(JsonConvert.SerializeObject(response, new JsonSerializerSettings { Formatting = Formatting.Indented }));
+        }
+
+        private ClaimsIdentity GetIdentity(string username, string password)
+        {
+            Person person = _people.FirstOrDefault(x => x.Login == username && x.Password == password);
+            if (person != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, person.Login),
+                    new Claim(ClaimsIdentity.DefaultRoleClaimType, person.Role)
+                };
+
+                var claimsIdentity = new ClaimsIdentity(claims, "Token", 
+                    ClaimsIdentity.DefaultNameClaimType,
+                    ClaimsIdentity.DefaultRoleClaimType);
+
+                return claimsIdentity;
+            }
+
+            // если пользователя не найдено
+            return null;
+        }
+    }
+}
